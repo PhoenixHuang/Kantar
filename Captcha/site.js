6f748966853b6a9017097384ae9374ecb855947b
phoenix huang <hzb2001@163.com>
2022-04-18 13:25:04 +0800

    Update site.js

--- Captcha/site.js
+++ Captcha/site.js
@@ -1,3 +1,16 @@
+var startX,startY; 
+document.addEventListener("touchstart",function(e){ 
+    startX = e.targetTouches[0].pageX;
+    startY = e.targetTouches[0].pageY;
+}); 
+document.addEventListener("touchmove",function(e){ 
+    var moveX = e.targetTouches[0].pageX;
+    var moveY = e.targetTouches[0].pageY;    
+    if(Math.abs(moveX-startX)>Math.abs(moveY-startY)){
+        e.preventDefault();
+    }
+},{passive:false});
+
 $(function () {
     $(".mrNext").hide();
     var captcha = sliderCaptcha({
